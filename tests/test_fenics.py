6393b0a361d2446f085e97d7de7d4337ac3aefd3
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-05-12 12:28:49 +0200

    Changed IntervalMesh to UnitIntervalMesh

--- tests/test_fenics.py
+++ tests/test_fenics.py
@@ -2,6 +2,6 @@ import fenics as pde
 
 
 def test_create_function_space():
-    mesh = pde.IntervalMesh(32)
+    mesh = pde.UnitIntervalMesh(32)
     V = pde.FunctionSpace(mesh, 'CG', 2)
     assert False
